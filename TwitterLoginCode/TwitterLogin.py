9d251feb7d0838b0295e5c848c90ee34607c6171
Paula Hernández <paulahernandez@dhcp-wifi-8021x-155-41-92-232.bu.edu>
2018-03-24 18:10:17 -0400

    Trying this out. Just changed one line

--- TwitterLoginCode/TwitterLogin.py
+++ TwitterLoginCode/TwitterLogin.py
@@ -6,6 +6,8 @@ from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
+# Just adding this comment to try git
+
 # configuration
 SECRET_KEY = 'development key'
 DEBUG = True
